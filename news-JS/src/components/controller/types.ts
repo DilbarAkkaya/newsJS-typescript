7fcac9d1c1fc9479cfeb6db2a33cbfd40065d4fa
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-13 17:59:48 +0300

    add isourse interface

--- news-JS/src/components/controller/types.ts
+++ news-JS/src/components/controller/types.ts
@@ -3,8 +3,13 @@ export interface IData {
   content: string,
   description: string,
   publishedAt: string,
-  source: string,
+  source: ISourse,
   title: string,
   url: string,
   urlToImage: string,
+}
+
+interface ISourse {
+  id: string,
+  name: string,
 }
