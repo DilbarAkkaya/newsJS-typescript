36822f7b33b9df0303f8176cd42ab9e61a42cdc9
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-13 14:41:01 +0300

    feat: add apikey from newsApi

--- news-JS/src/components/controller/appLoader.js
+++ news-JS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2acbde45657e45608e9ef56423cffd57',
         });
     }
 }
