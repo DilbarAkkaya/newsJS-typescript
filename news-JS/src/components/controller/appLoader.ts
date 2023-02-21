cabc6bbbfdc428b509f8c0065fa2dd42e59f981a
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-21 14:50:10 +0300

    fix: fix deploy link

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '2acbde45657e45608e9ef56423cffd57',
         });
     }
